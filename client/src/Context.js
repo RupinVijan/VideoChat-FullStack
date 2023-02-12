4a7e09e073ad04182abbbf692fc7acdefb6e8c36
Rupin Vijan <rupinvijan@Rupins-MacBook-Air.local>
2023-02-12 11:58:25 +0530

    api updated

--- client/src/Context.js
+++ client/src/Context.js
@@ -4,8 +4,8 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-// const socket = io('http://localhost:5000');
-const socket = io('https://warm-wildwood-81069.herokuapp.com');
+const socket = io('http://43.205.232.242');
+// const socket = io('https://warm-wildwood-81069.herokuapp.com');
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
